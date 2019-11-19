6476448d4c7b44d9e415a8a6f8c9555cbe289d83
Kristin Baday <kristinsbdy@gmail.com>
2019-11-19 23:44:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ function add(a,b){
     var something = a+b;
     return(something)
 }
-fu
+function subtract(a,b){
+    var something = b-a;
+    return(something)
+}
+function subtract(a,b){
+    var something = b*a;
+    return(something)
+}
+function inc(n){
+  var newthing = n+1;
+  return(newthing)
+}
+function dec(n){
+  var newthing = n-1;
+  return(newthing)
+}
+function makeInt(n){
+  var newthing = parseInt(n,1);
+  return(newthing)
+}
